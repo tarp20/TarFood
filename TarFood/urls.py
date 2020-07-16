f35c676e9d073d4a0c351622b6d28758125c0771
Taras Piont <taras.piont@gmail.com>
2020-07-16 12:40:04 +0200

    bu fix

--- TarFood/urls.py
+++ TarFood/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
-    path('restaurant/sing-in', LoginView.as_view(),
+    path('restaurant/sing-in/', LoginView.as_view(),
          {'template_name' : 'restaurant/sing-in.html'},
          name ='restaurant-sing-in'),
 
