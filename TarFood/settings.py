b6dbe854a79f21eadbc544b3df489c88e2b4ae0c
Taras Piont <taras.piont@gmail.com>
2020-07-22 13:12:56 +0200

    fixed  request bug

--- TarFood/settings.py
+++ TarFood/settings.py
@@ -155,16 +155,19 @@ SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
 }
 
 
+
+
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
     'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.auth_allowed',
-    'TarFoodApp.pipeline.load_user',
     'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'TarFoodApp.social_auth_pipeline.create_user_by_type',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
-
 
 
--- TarFoodApp/social_auth_pipeline.py
+++ TarFoodApp/social_auth_pipeline.py
@@ -1,11 +1,14 @@
 from TarFoodApp.models import Customer,Driver
 
+def create_user_by_type(backend , user , response , *args, **kwargs):
+    request = backend.strategy.request_data()
 
-def create_user_by_type(backend,user,response,*args,**kwargs):
     if backend.name == 'facebook':
-        avatar = 'http://graph.facebook.com/%s/picture?type=large' % response ['id']
-    if request['user_type'] == "driver" and not Driver.objects.filter(user_id = user.id):
+        avatar = 'http://graph.facebook.com/%s/picture?type=large' % response['id']
+    if request['user_type'] == 'driver' and not Driver.objects.filter(user_id = user.id):
         Driver.objects.create(user_id=user.id,avatar=avatar)
+    elif not Customer.objects.filter(user_id=user.id):
+        Customer.objects.create(user_id=user.id, avatar=avatar)
 
 
 
