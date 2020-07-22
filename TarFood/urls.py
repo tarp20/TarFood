41d2153393ffa97de9967c1bdbc53efcb689fe34
Taras Piont <taras.piont@gmail.com>
2020-07-22 21:54:52 +0200

    added  css settings

--- TarFood/urls.py
+++ TarFood/urls.py
@@ -17,6 +17,13 @@ urlpatterns = [
     path('restaurant/sign-out/', auth_views.LogoutView.as_view(next_page = '/'),name = 'restaurant-sign-out'),
     path('restaurant/sign-up/', views.restaurant_sign_up, name='restaurant-sign-up'),
     path('restaurant/',views.restaurant_home,name = 'restaurant-home'),
+
+    path('restaurant/account/', views.restaurant_account, name = 'restaurant-account'),
+    path('restaurant/meal/', views.restaurant_meal, name = 'restaurant-meal'),
+    path('restaurant/order/', views.restaurant_order, name = 'restaurant-order'),
+    path('restaurant/report/', views.restaurant_report, name = 'restaurant-report'),
+
+
     path('api/social/', include('rest_framework_social_oauth2.urls')),
 
 
