4bdb7ab6d270df96527b503f98ac4f86836ba259
Taras Piont <taras.piont@gmail.com>
2020-07-27 15:37:06 +0200

    added orders models

--- TarFoodApp/admin.py
+++ TarFoodApp/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 
-from TarFoodApp.models import Restaurant,Customer,Driver,Meal
+from TarFoodApp.models import Restaurant,Customer,Driver,Meal,Order,OrderDetails
 
 admin.site.register(Restaurant)
 admin.site.register(Customer)
 admin.site.register(Driver)
 admin.site.register(Meal)
+admin.site.register(Order)
+admin.site.register(OrderDetails)
+
+
 
--- TarFoodApp/models.py
+++ TarFoodApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Restaurant(models.Model):
@@ -42,6 +43,41 @@ class Meal(models.Model):
 
     def __srt__(self):
         return self.name
+
+
+class Order(models.Model):
+    COOCKING = 1
+    READY = 2
+    ONTHEWAY = 3
+    DELIVERED = 4
+
+    STATUS_CHOICES = (
+        (COOCKING,"COOCKING"),
+        (READY, "READY"),
+        (ONTHEWAY, "ONTHEWAY"),
+        (DELIVERED, "DELIVERED"),
+    )
+
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant,on_delete=models.CASCADE)
+    driver = models.ForeignKey(Driver,on_delete=models.CASCADE)
+    address = models.CharField(max_length=500)
+    total = models.IntegerField()
+    status = models.IntegerField(choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(default=timezone.now)
+    picked_at = models.DateTimeField(blank = True,null=True)
+
+
+class OrderDetails(models.Model):
+    order = models.ForeignKey(Order,related_name='order_details',on_delete=models.CASCADE)
+    meal = models.ForeignKey(Meal,on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    sub_total = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id)
+
+
 
 
 
